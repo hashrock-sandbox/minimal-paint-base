6e95ecaca3f96c99cc0d750de3165c77671545b2
hashrock <hashrock@users.noreply.github.com>
2022-09-03 20:39:07 +0900

    fix hsv

--- src/Color.vue
+++ src/Color.vue
@@ -1,15 +1,7 @@
 <template>
-  <svg viewBox="0 0 200 100" width="200" height="100" ref="canv">
+  <svg viewBox="0 0 200 300" width="200" height="300" ref="canv">
     <defs>
-      <linearGradient
-        id="hsv1"
-        x1="0%"
-        y1="0%"
-        x2="100%"
-        y2="0%"
-        gradientUnits="userSpaceOnUse"
-        spreadMethod="repeat"
-      >
+      <linearGradient id="hsv1" x1="0%" y1="0%" x2="100%" y2="0%" gradientUnits="objectBoundingBox" spreadMethod="repeat">
         <stop offset="0%" stop-color="#ff0000" />
         <stop offset="16.7%" stop-color="#ffff00" />
         <stop offset="33.3%" stop-color="#00ff00" />
@@ -18,44 +10,28 @@
         <stop offset="83.3%" stop-color="#ff00ff" />
         <stop offset="100%" stop-color="#ff0000" />
       </linearGradient>
-      <linearGradient
-        id="hsv2"
-        x1="0%"
-        y1="0%"
-        x2="100%"
-        y2="0%"
-        gradientUnits="userSpaceOnUse"
-        spreadMethod="repeat"
-      >
-        <stop offset="0%" :stop-color="sStop[0]" />
-        <stop offset="100%" :stop-color="sStop[1]" />
+      <linearGradient id="hsv2" x1="0%" y1="0%" x2="100%" y2="0%" gradientUnits="objectBoundingBox" spreadMethod="repeat">
+        <stop offset="0%" :stop-color="slBox[0]" />
+        <stop offset="100%" :stop-color="slBox[1]" />
       </linearGradient>
-      <linearGradient
-        id="hsv3"
-        x1="0%"
-        y1="0%"
-        x2="100%"
-        y2="0%"
-        gradientUnits="userSpaceOnUse"
-        spreadMethod="repeat"
-      >
-        <stop offset="0%" :stop-color="lStop[0]" />
-        <stop offset="50%" :stop-color="lStop[1]" />
-        <stop offset="100%" :stop-color="lStop[2]" />
+      <linearGradient id="hsv3" x1="0%" y1="0%" x2="0%" y2="100%" gradientUnits="objectBoundingBox" spreadMethod="repeat">
+        <stop offset="0%" :stop-color="slBox[2]" />
+        <stop offset="100%" :stop-color="slBox[3]" />
       </linearGradient>
     </defs>
-    <rect rx="20" :fill="hsl" x="0" y="0" width="20" height="20" />
-    <rect
-      fill="url(#hsv1)"
-      x="0"
-      y="20"
-      height="20"
-      width="200"
-      @pointermove="moveH"
-      @pointerup="up"
-      @pointerdown="downH"
-    />
-    <rect
+    <rect rx="20" :fill="hsl" x="0" y="260" width="20" height="20" />
+    <g>
+      <rect x="0" y="0" width="200" height="200" fill="url(#hsv2)" />
+      <rect x="0" y="0" width="200" height="200" fill="url(#hsv3)"  @pointermove="moveSV" @pointerup="up" @pointerdown="downSV" />
+      <!-- <rect x="0" y="0" width="200" height="200" fill="none" /> -->
+      <circle style="pointer-events: none;" :cx="s * 200 / 100" :cy="(100 - v) * 200 / 100" r="5" stroke="black" fill="white" />
+    </g>
+    <g transform="translate(0, 200)">
+      <rect fill="url(#hsv1)" x="0" y="0" height="20" width="200" @pointermove="moveH" @pointerup="up"
+        @pointerdown="downH" />
+      <circle style="pointer-events: none;" :cx="h * 200 / 360" cy="10" r="5" stroke="black" fill="white" />
+    </g>
+    <!-- <rect
       fill="url(#hsv2)"
       x="0"
       y="40"
@@ -74,35 +50,16 @@
       @pointermove="moveL"
       @pointerup="up"
       @pointerdown="downL"
-    />
-    <circle
-      style="pointer-events: none;"
-      :cx="h * 200 / 360"
-      cy="30"
-      r="5"
-      stroke="black"
-      fill="white"
-    />
-    <circle
-      style="pointer-events: none;"
-      :cx="s * 200 / 100"
-      cy="50"
-      r="5"
-      stroke="black"
-      fill="white"
-    />
-    <circle
-      style="pointer-events: none;"
-      :cx="l * 200 / 100"
-      cy="70"
-      r="5"
-      stroke="black"
-      fill="white"
-    />
+    /> -->
+
   </svg>
 </template>
 
 <script>
+// https://stackoverflow.com/questions/3423214/convert-hsb-hsv-color-to-hsl/54116681#54116681
+const hsl2hsv = (h,s,l,v=s*Math.min(l,1-l)+l) => [h, v?2-2*l/v:0, v];
+const hsv2hsl = (h,s,v,l=v-v*s/2, m=Math.min(l,1-l)) => [h,m?(v-l)/m:0,l];
+
 export default {
   props: {
     initialColor: Object
@@ -111,7 +68,7 @@ export default {
     return {
       h: 50,
       s: 100,
-      l: 50,
+      v: 50,
       pressed: false
     };
   },
@@ -122,7 +79,16 @@ export default {
   },
   computed: {
     hsl() {
-      return `hsl(${this.h}, ${this.s}%, ${this.l}%)`;
+      const [h, s, l] = hsv2hsl(this.h, this.s / 100, this.v / 100);
+      return `hsl(${h.toFixed(0)}, ${(s * 100).toFixed(0)}%, ${(l * 100).toFixed(0)}%)`;
+    },
+    slBox() {
+      return [
+        `hsla(${this.h}, 0%, 100%, 1)`,
+        `hsla(${this.h}, 100%, 50%, 1)`,
+        `hsla(${this.h}, 100%, 0%, 0)`,
+        `hsla(${this.h}, 0%, 0%, 1)`,
+      ]
     },
     sStop() {
       return [
@@ -146,6 +112,13 @@ export default {
       pt.y = ev.clientY;
       return pt.matrixTransform(el.getScreenCTM().inverse()).x;
     },
+    getY(ev) {
+      const el = this.$refs.canv;
+      const pt = el.createSVGPoint();
+      pt.x = ev.clientX;
+      pt.y = ev.clientY;
+      return pt.matrixTransform(el.getScreenCTM().inverse()).y;
+    },    
     downH(ev) {
       ev.target.setPointerCapture(ev.pointerId);
       this.pressed = true;
@@ -157,37 +130,31 @@ export default {
       }
       this.h = (this.getX(ev) / 200) * 360;
     },
-    downS(ev) {
-      ev.target.setPointerCapture(ev.pointerId);
-      this.pressed = true;
-      this.moveS(ev);
-    },
-    moveL(ev) {
+    moveSV(ev) {
       if (!this.pressed) {
         return;
       }
-      this.l = (this.getX(ev) / 200) * 100;
+      this.v = 100 - (this.getY(ev) / 200) * 100;
+      this.s = (this.getX(ev) / 200) * 100
     },
-    downL(ev) {
+    downSV(ev) {
       ev.target.setPointerCapture(ev.pointerId);
       this.pressed = true;
-      this.moveL(ev);
-    },
-    moveS(ev) {
-      if (!this.pressed) {
-        return;
-      }
-      this.s = (this.getX(ev) / 200) * 100;
+      this.moveSV(ev);
     },
     up(ev) {
       this.pressed = false;
     }
   },
   mounted() {
+    console.log("mounted")
+
     if (this.initialColor) {
-      this.h = this.initialColor.h;
-      this.s = this.initialColor.s;
-      this.l = this.initialColor.l;
+      const [h, s, v] = hsl2hsv(this.initialColor.h, this.initialColor.s, this.initialColor.l);
+
+      this.h = h;
+      this.s = s;
+      this.v = v;
     }
   }
 };
